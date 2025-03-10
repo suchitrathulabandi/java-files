e932b79711b2419ae29fd2a41e83152a6e628a68
suchitrathulabandi <suchitrathulabandi2003@gmail.com>
2025-03-10 11:55:35 +0530

    first commit

---
+++ PositiveOrNegativeNumber.java
@@ -0,0 +1,12 @@
+public class PositiveOrNegativeNumber {
+    public static void main (String[]args){
+    int num = 0;
+    if (num > 0)
+        System.out.println ("The number is positive");
+    else if (num < 0)
+        System.out.println ("The number is negative");
+    else
+        System.out.println ("Zero");
+  }
+    
+}
