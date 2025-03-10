46bbc74b27c79ad573ed9b3324bb417af4372446
suchitrathulabandi <suchitrathulabandi2003@gmail.com>
2025-03-10 13:08:48 +0530

    first commit

---
+++ SumOfNaturalNumbersInGivenRange.java
@@ -0,0 +1,12 @@
+public class SumOfNaturalNumbersInGivenRange {
+    public static void main(String[] args){
+        int n1=5;
+        int n2=10;
+        int sum=0;
+        for (int i=n1; i<=n2; i++){
+            sum=sum+i;
+}
+        System.out.println("the sum of natural numbers in a given range is:" +sum);
+    }
+    
+}
