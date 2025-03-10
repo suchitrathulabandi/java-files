0dfd615f21e972a7e2595e892714c42e2c4a7ef8
suchitrathulabandi <suchitrathulabandi2003@gmail.com>
2025-03-10 12:27:55 +0530

    first commit

---
+++ SumOfNnaturalNumbers.java
@@ -0,0 +1,10 @@
+public class SumOfNnaturalNumbers {
+    public static void main(String[] args){
+        int n=10;
+        int sum=0;
+        for (int i=1; i<=n; i++)
+        sum=sum+i;
+        System.out.println("the sum of first N natural numbers is:" +sum);
+    }
+    
+}
